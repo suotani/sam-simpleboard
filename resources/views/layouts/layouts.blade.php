274f8a2cde73a6a52d2a09a5cb12481c0f50378f
satoshi uotani <s.uotani.zetakansu@gmail.com>
2020-10-01 16:59:38 +0900

    to responsive

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,6 +2,7 @@
   <head>
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
 
@@ -15,7 +16,7 @@
 
     @component('components.footer')
     @endcomponent
-    
+
     <script src="{{asset('js/app.js')}}"></script>
   </body>
 </html>
