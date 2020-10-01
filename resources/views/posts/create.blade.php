803e0c2970d3cd08caf0ff8c2c4a0e3ceabc90cc
satoshi uotani <s.uotani.zetakansu@gmail.com>
2020-10-01 16:56:32 +0900

    add validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,16 +4,24 @@
 @section('content')
 
 <h1>New Post</h1>
+@if($errors->any())
+<div class="alert alert-danger">
+  <ul>
+    @foreach($errors->all() as $error)
+      <li>{{$error}}</li>
+    @endforeach
+</div> 
+@endif
 
 <form method="POST" action="/posts">
     {{ csrf_field() }}
     <div class="form-group">
         <label for="exampleInputEmail1">Title</label>
-        <input type="text" class="form-control" aria-describedby="emailHelp" name="title">
+        <input type="text" class="form-control" aria-describedby="emailHelp" name="title" value="{{old('title')}}">
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Content</label>
-        <textarea class="form-control" name="content"></textarea>
+        <textarea class="form-control" name="content">{{old('content')}}</textarea>
     </div>
     <button type="submit" class="btn btn-outline-primary">Submit</button>
 </form>
