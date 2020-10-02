e0a259cbec266faa420ea0f082cf395c4bf05b6d
satoshi uotani <s.uotani.zetakansu@gmail.com>
2020-10-02 09:20:47 +0900

    fix for deploy

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,7 +3,7 @@
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{csrf_token()}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
   </head>
 
   <body>
@@ -19,6 +19,6 @@
     @component('components.footer')
     @endcomponent
 
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{mix('js/app.js')}}"></script>
   </body>
 </html>
